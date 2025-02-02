67ff118d2a65fd8a50cfaed241deeeede85c9321
Joshclxx <joshuacolobong11@gmail.com>
2025-02-02 22:16:19 +0800

    added function to the toggle menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Button from "@/components/Button";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="w-full bg-gradient-1 relative z-30 py-5">
       <div className="max-w-[1440px] mx-auto flexBetween padding-container">
@@ -38,7 +47,26 @@ const Navbar = () => {
           width={32}
           height={32}
           className="inline-block cursor-pointer lg:hidden"
+          onClick={toggleMenu}
         />
+      </div>
+
+      <div
+        className={`lg:hidden ${isMenuOpen ? "block" : "hidden"} absolute left-0 w-full bg-gradient-1 py-5 mt-3 z-20`}
+      >
+      <ul className="flex flex-col items-center gap-6">
+        {NAV_LINKS.map((link) => (
+        <Link
+          href={link.href}
+          key={link.key}
+          className="text-[#0A1647] text-xl font-semibold"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          {link.label}
+        </Link>
+        ))}
+
+      </ul>
       </div>
     </nav>
   );
